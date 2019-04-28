0e4c4b037e8236059ec70f31699dac5aa86462e9
hjacquir <jacquirhatim@gmail.com>
2019-04-28 08:02:01 +0200

    ajout des exceptions

--- src/Hj/Exception/FileNotFoundException.php
+++ src/Hj/Exception/FileNotFoundException.php
@@ -1,14 +1,8 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: monpc
- * Date: 28/04/2019
- * Time: 07:32
- */
 
 namespace Hj\Exception;
 
-
-class FileNotFoundException {
+class FileNotFoundException extends \Exception implements HjException
+{
 
 }
--- src/Hj/Exception/HjException.php
+++ src/Hj/Exception/HjException.php
@@ -1,14 +1,8 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: monpc
- * Date: 28/04/2019
- * Time: 07:31
- */
 
 namespace Hj\Exception;
 
-
-interface HjException {
+interface HjException
+{
 
 }
